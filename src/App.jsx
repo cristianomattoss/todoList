63d20eab9ce218d4b2029c4c73a338f5aafdc233
Cristiano Mattos <mateuscmattos@gmail.com>
2026-08-11 17:18:56 -0300

    feat: implement task creation

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,18 @@ import CreateTask from './components/CreateTask'
 import ListTask from './components/ListTask'
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const createTask = (task) => {
+    const newTasks = [...tasks, task];
+    setTasks(newTasks);
+    console.log(newTasks);
+  }
+
   return (
     <div className="container">
       <h1>Minha lista de tarefas</h1>
-      <CreateTask/>
+      <CreateTask createTask={createTask}/>
       <ListTask/>
     </div>
   )
--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -1,13 +1,27 @@
 import { FaPlus } from "react-icons/fa";
 import "./CreateTask.css";
+import { useState } from "react";
 
-const CreateTask = () => {
+const CreateTask = ({createTask}) => {
+  const [task, setTask] = useState("");
+
+  const handleTaskChange = (e) => {
+    setTask(e.target.value);
+  }
+
+  const handleCreateTask = () => {
+    if (!task.trim()) return;
+
+    createTask(task);
+    setTask("");
+  }
+
   return (
     <div id="container-create-task">
         <label htmlFor="task">Insira uma nova tarefa:</label>
         <div id="task-input-container">
-            <input type="text" name="task" id="task"/>
-            <button id="add-task">
+            <input type="text" name="task" id="task" value={task} onChange={handleTaskChange}/>
+            <button id="add-task" onClick={handleCreateTask}>
                 <FaPlus/>
             </button>
         </div>
