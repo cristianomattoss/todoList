5f07020030ee34f6763678e3fc04ca93abcb4e4d
Cristiano Mattos <mateuscmattos@gmail.com>
2026-08-11 22:25:54 -0300

    feat: search and filter tasks

--- src/components/ListTask.jsx
+++ src/components/ListTask.jsx
@@ -1,18 +1,58 @@
 import { FaEdit } from "react-icons/fa";
 import { FaTrash } from "react-icons/fa";
 import "./ListTask.css"
+import { useEffect, useState } from "react";
 
 const ListTask = ({tasks, toggleTask, removeTask}) => {
+    const [searchValueInput, setSearchValue] = useState("");
+    const [filterValueInput, setFilterValue] = useState("all");
+    const [filterTasks, setFilterTasks] = useState(tasks);
+
+    useEffect(() => {searchTask(searchValueInput, filterValueInput);}, [tasks]);
+    
+    const searchTask = (searchValue, filterValue) => {
+        let filteredTasks;
+        switch(filterValue){
+            case "all":
+                filteredTasks = tasks;
+                break;
+            case "done":
+                filteredTasks = tasks.filter(task => task.completed == true)
+                break;
+            case "todo":
+                filteredTasks = tasks.filter(task => task.completed == false)
+                break;
+        }
+
+        if(searchValue.trim()) {
+            filteredTasks = filterTasks.filter(task => task.text.toLowerCase().includes(searchValueInput.toLowerCase()))
+        }
+        setFilterTasks(filteredTasks)
+        
+    }
+
   return (
     <div id='container-list'>
         <div id="container-search">
             <div id="search-input-container">
                 <label htmlFor="search">Pesquisar:</label>
-                <input type="text" id='search' name='search'/>
+                <input type="text" id='search' name='search'  value={searchValueInput} 
+                    onChange={(e) => {
+                        const value = e.target.value;
+                        setSearchValue(value);
+                        searchTask(value, filterValueInput); 
+                    }}
+                />
             </div>
             <div id='filter-select-container'>
                 <label htmlFor="filter-select">Filtrar:</label>
-                <select name="filter" id="filter-select">
+                <select name="filter" id="filter-select" value={filterValueInput}
+                    onChange={(e) => {
+                        const value = e.target.value;
+                        setFilterValue(value);
+                        searchTask(searchValueInput, value); 
+                    }}
+                >
                     <option value="all">Todos</option>
                     <option value="done">Feitos</option>
                     <option value="todo">A fazer</option>
@@ -20,7 +60,7 @@ const ListTask = ({tasks, toggleTask, removeTask}) => {
             </div>
         </div>
         <div id='list-tasks'>
-            {tasks.map((task, index) => (
+            {filterTasks.map((task, index) => (
                 <div className="task" key={index}>
                     <input 
                         type="checkbox"
